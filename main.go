17fb7809b192cad2466f53f643349d657f0187a4
User1 Name <user1@example.com>
2025-05-25 20:38:05 -0400

    :sparkles: feat: Adiciona arquivos ao repo, consome fila, envia email.

---
+++ main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/streadway/amqp"
+)
+
+func main() {
+	//conn, err := amqp.Dial(os.Getenv("URL_FILA"))
+	//nomeFila := os.Getenv("NOME_FILA")
+
+	//=====================TESTE ==========================
+	conn, err := amqp.Dial("amqp://guest:guest@192.168.1.31:5672/")
+	nomeFila := "exames-pendentes"
+	//=====================================================
+
+	if err != nil {
+		log.Fatalf("failed to connect to RabbitMQ: %v", err)
+	}
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	if err != nil {
+		log.Fatalf("failed to open a channel: %v", err)
+	}
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		nomeFila,
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+	if err != nil {
+		log.Fatalf("failed to declare a queue: %v", err)
+	}
+
+	msgs, err := ch.Consume(
+		q.Name,
+		"",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+	if err != nil {
+		log.Fatalf("failed to register a consumer: %v", err)
+	}
+
+	sigchan := make(chan os.Signal, 1)
+	signal.Notify(sigchan, syscall.SIGINT, syscall.SIGTERM)
+
+	forever := make(chan bool)
+
+	go func() {
+		 log.Println("Consumer iniciado. Aguardando mensagens...")
+		for d := range msgs {
+			log.Printf("message received: %v", string(d.Body))
+			EnviarEmail(string(d.Body))
+		}
+	}()
+
+	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+	<-sigchan
+
+	log.Printf("interrupted, shutting down")
+	forever <- true
+}
---
+++ utils.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/smtp"
+)
+
+type Agendamentos struct {
+	DataHora      string `json:"data_hora"`
+	NomeExame     string `json:"nome_exame"`
+	Instrucoes    string `json:"instrucoes"`
+	Paciente      string `json:"nome_paciente"`
+	EmailPaciente string `json:"email_paciente"`
+}
+
+func EnviarEmail(agendamentoJson string) {
+
+	var agendamento Agendamentos
+
+	err := json.Unmarshal([]byte(agendamentoJson), &agendamento)
+	if err != nil {
+		fmt.Println("Erro ao fazer o unmarshall do JSON:", err)
+	}
+
+	// Configurações do servidor SMTP
+	from := "examedsd@gmail.com"
+	senha_app := "ywcs uxja mjrm ziit"
+	body := MontaMensagem(agendamento)
+	to := []string{agendamento.EmailPaciente}
+
+	errr := smtp.SendMail("smtp.gmail.com:587", smtp.PlainAuth("", from, senha_app, "smtp.gmail.com"),
+		from,
+		to,
+		[]byte(body))
+
+	if errr != nil {
+		fmt.Print(errr)
+	}
+
+	// Exemplo de impressão dos dados do agendamento
+	fmt.Println("Enviando e-mail com os seguintes dados:")
+	fmt.Printf("Data e Hora: %s\n", agendamento.DataHora)
+	fmt.Printf("ID do Exame: %s\n", agendamento.NomeExame)
+	fmt.Printf("Instruções: %s\n", agendamento.Instrucoes)
+	fmt.Printf("Nome do Paciente: %s\n", agendamento.Paciente)
+	fmt.Printf("Email do Paciente: %s\n", agendamento.EmailPaciente)
+}
+
+func MontaMensagem(agendamento Agendamentos) string {
+
+	msg := fmt.Sprintf(`Olá, %s! Tudo bem?
+	Somos da EXAMED e gostaríamos de informar que o seu exame de %s foi agendado com sucesso para o dia %s. 
+	Para que tudo ocorra bem, recomendamos que voce siga as seguintes instruções e preparativos:
+	%s`, agendamento.Paciente, agendamento.NomeExame, agendamento.DataHora, agendamento.Instrucoes)
+
+	return msg
+}
